0015713ed6bd5bf98314283b2b8a8786aa22b1ec
bummings <eabummings@gmail.com>
2019-04-11 22:29:21 -0400

    maybe don't worry about nodemon on a CRA instance

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>I noticed yr localhost is movin slow as hell</h1>
+        <h1>Hm, that might have something to do with it</h1>
       </div>
     );
   }
