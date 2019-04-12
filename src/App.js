7cbf3078b4cb6379a160462b3869fb48e0c22515
bummings <eabummings@gmail.com>
2019-04-11 22:34:01 -0400

    beginning sass integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hm, that might have something to do with it</h1>
+        <h1>Install SASS structure</h1>
       </div>
     );
   }
