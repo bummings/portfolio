f9afd435e7a829de69ff939b41e508190e803d25
bummings <eabummings@gmail.com>
2019-04-11 22:27:17 -0400

    localhost movin slow as hell?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div className='App'>bruh</div>;
+    return (
+      <div className='App'>
+        <h1>I noticed yr localhost is movin slow as hell</h1>
+      </div>
+    );
   }
 }
 
