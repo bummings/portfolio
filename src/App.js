ff4f672d6394a699fd369f66ea2953714ed9ab90
bummings <eabummings@gmail.com>
2019-04-17 12:07:30 -0400

    creates codeheader component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CodeHeader from './components/CodeHeader';
+
 import './App.scss';
 
 class App extends Component {
@@ -7,6 +9,7 @@ class App extends Component {
       <div className='App'>
         <div className='landing'>
           <h1 className='landing__header'>Install SASS structure</h1>
+          <CodeHeader />
         </div>
       </div>
     );
---
+++ src/components/CodeHeader.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CodeHeader extends Component {
+  render() {
+    return <div>bruh!</div>;
+  }
+}
+
+export default CodeHeader;
