0aa7354d1faa7c0ea34a9129ef985d5effa37ed6
bummings <eabummings@gmail.com>
2019-04-21 21:32:56 -0400

    mmhm

--- src/components/Hi.js
+++ src/components/Hi.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Hi extends Component {
   render() {
     return (
-      <div>
-        <h1 />
+      <div className='hi'>
+        <h1 className='hi__header'>Hi, I'm Edd-</h1>
+        <h1 className='hi__header'>
+          a full-stack developer from Philadelphia.
+        </h1>
       </div>
     );
   }
