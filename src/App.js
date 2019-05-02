612f8f0a83213db964883b235ad9fee3e63272c8
bummings <eabummings@gmail.com>
2019-05-02 14:41:55 -0400

    now you count my PRs, yeah?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import Splash from './components/Splash';
 import CodeHeader from './components/Landing';
 import Hi from './components/Hi';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 import './App.scss';
 
@@ -8,8 +11,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Splash />
         <CodeHeader />
         <Hi />
+        <Projects />
+        {/* <Footer /> */}
       </div>
     );
   }
--- src/components/Hi.js
+++ src/components/Hi.js
@@ -4,14 +4,13 @@ class Hi extends Component {
   render() {
     return (
       <div className='hi'>
-        <h1 className='hi__header'>Hello!</h1>
-        <h1 className='hi__header'>My name is Edd Burke</h1>
+        <h1 className='hi__header'>Hello! My name is Edd Burke.</h1>
         <p className='hi__paragraph'>
-          and I am a profoundly-driven programmer, developer, designer, hacker
-          and- above all- learner. Software and technology has been the
-          consistent source of inspiration of my life: learning to solve my own
-          personal quagmires and working with the greatest community of all in
-          fellow futurists- all the while learning with great pleasure every
+          I am a profoundly-driven programmer, developer, designer, hacker and-
+          above all- learner. Software and technology has been the consistent
+          source of inspiration of my life: learning to solve my own personal
+          quagmires and working with the greatest community of all in fellow
+          futurists- all the while learning with great pleasure every
           functional, modular development and design pattern I can sink my teeth
           into.
         </p>
