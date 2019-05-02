bbd642dd3ede7cc58cd0df44b4e9a4ce26118b94
bummings <eabummings@gmail.com>
2019-05-02 16:19:32 -0400

    ok, this is all gonna be on header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Splash />
-        <CodeHeader />
+        {/* <CodeHeader /> */}
         <Hi />
         <Projects />
         {/* <Footer /> */}
