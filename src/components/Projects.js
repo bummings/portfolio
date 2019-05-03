8b7c4bc18ddc52e8a9f3f2bb32c6f6a9c4858106
bummings <eabummings@gmail.com>
2019-05-03 01:55:35 -0400

    getting the projects sorted

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,8 +13,22 @@ class Projects extends Component {
       <div className='projects'>
         <h1 className='projects__header'>Projects</h1>
         <div className='projects__project'>
-          <img src={bonafind} alt='' className='projects__project--img' />
-          <h2 className='projects__project--header'>Bonafind</h2>
+          <div className='projects__project--one'>
+            <img src={bonafind} alt='' className='projects__project--img' />
+          </div>
+          <div className='projects__project--two'>
+            <h2 className='projects__project--header'>Bonafind</h2>
+            <p className='projects__project--description'>
+              Bonafind is a web application that helps you discover restaurants
+              and businesses near you. Users can leave personalized reviews,
+              find hours, photos and contact information related to the
+              businesses.
+            </p>
+            <p className='projects__project--description'>
+              This application was developed in React and NodeJS and PostgresQL
+              backend, utilizing the Google Places API.
+            </p>
+          </div>
         </div>
       </div>
     );
