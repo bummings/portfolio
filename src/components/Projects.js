8b37b10a72d5e3577295f62038a04d00147b4c04
bummings <eabummings@gmail.com>
2019-05-03 02:14:57 -0400

    eddnotes added, too

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,7 @@ class Projects extends Component {
     return (
       <div className='projects'>
         <h1 className='projects__header'>Projects</h1>
+        {/* !!! */}
         <div className='projects__project'>
           <div className='projects__project--one'>
             <img src={bonafind} alt='' className='projects__project--img' />
@@ -36,6 +37,28 @@ class Projects extends Component {
               integration. This application also utilized the Google Places API
               for business data scraping.
             </p>
+          </div>
+        </div>
+        {/* !!! */}
+        <div className='projects__project'>
+          <div className='projects__project--two'>
+            <h2 className='projects__project--header'>Edd Notes</h2>
+            <h3 className='projects__project--sub'>October 2018</h3>
+
+            <p className='projects__project--description'>
+              A fundamental note-taking web application that helped me
+              understand the finer details of lacing a React front end to a
+              RESTful API built from scratch.
+            </p>
+            <p className='projects__project--description'>
+              The front end was built in React and a combination of Sass and
+              Styled Components on the front end, NodeJS and Hapi.js on the
+              backend, connected to a SQLite3 database. It was deployed to
+              Netlify and Heroku using TravisCI for continuous integration.
+            </p>
+          </div>
+          <div className='projects__project--one'>
+            <img src={eddnotes} alt='' className='projects__project--img' />
           </div>
         </div>
       </div>
