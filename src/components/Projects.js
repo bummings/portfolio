1e1b44028d945609be4b0ed6104e27a642d3b580
bummings <eabummings@gmail.com>
2019-05-03 01:47:30 -0400

    yeh ok you got some images now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import bonafind from '../assets/bonafind.jpg';
 class Projects extends Component {
   render() {
     return (
       <div className='projects'>
-        <h1 className='projects__header'>Bruh</h1>
+        <h1 className='projects__header'>Projects</h1>
+        <div className='projects__project'>
+          <img src={bonafind} alt='' className='projects__project--img' />
+          <h2 className='projects__project--header'>Bonafind</h2>
+        </div>
       </div>
     );
   }
