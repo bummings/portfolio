7bcecae2068dcf5d57d7bef5c321af96f3f8a53e
bummings <eabummings@gmail.com>
2019-05-03 02:08:35 -0400

    adds bonafind

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,15 +18,23 @@ class Projects extends Component {
           </div>
           <div className='projects__project--two'>
             <h2 className='projects__project--header'>Bonafind</h2>
+            <h3 className='projects__project--sub'>December 2018</h3>
+
             <p className='projects__project--description'>
               Bonafind is a web application that helps you discover restaurants
               and businesses near you. Users can leave personalized reviews,
               find hours, photos and contact information related to the
-              businesses.
+              businesses. A user can upgrade their account for access to premium
+              features such as leaving more than 3 reviews at a time.
             </p>
             <p className='projects__project--description'>
-              This application was developed in React and NodeJS and PostgresQL
-              backend, utilizing the Google Places API.
+              This application was developed in React for the front end, NodeJS
+              and Express on the backend and PostgresQL for the database. It
+              features OAuth for authentication and Stripe integration for
+              premium functionality. The frontend is deployed to Netlify and the
+              backend is deployed to Heroku, wired with TravisCI for continuous
+              integration. This application also utilized the Google Places API
+              for business data scraping.
             </p>
           </div>
         </div>
