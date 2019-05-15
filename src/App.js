c98b7167b3afb4662c70c785515464328a547a5d
bummings <eabummings@gmail.com>
2019-05-15 15:10:16 -0400

    adds svg for social icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Splash from './components/Splash';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+
 // import Footer from './components/Footer';
 import './App.scss';
 
