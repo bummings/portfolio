a1e4f9f8a00456bb416c7fbd440a941274ac1b4b
bummings <hello@bummings.io>
2022-05-26 17:22:19 -0400

    removes form and broadens icons for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,63 +11,27 @@ class Contact extends Component {
         <div className='contact__header'>
           <p
             className='contact__header--text'
-            data-aos='zoom-in'
-            data-aos-easing='ease-in-out'
-            data-aos-duration='1000'
           >
-            Get in touch
+            Let's talk!
           </p>
-          <span className='contact__header--invitation'>
-            Let's build something together.
-          </span>
         </div>
 
-        <div className='contact__split'>
-          {/* F O R M */}
-          <div className='contact__split--form'>
-            <div className='form'>
-              <form name='contact' method='POST' data-netlify='true'>
-                <div className='form__section'>
-                  <label className='form__label'>Name</label>
-                  <br />
-                  <input className='form__input' type='text' name='name' />
-                </div>
-
-                <div className='form__section'>
-                  <label className='form__label'>E-mail</label>
-                  <br />
-                  <input className='form__input' type='text' name='email' />
-                </div>
-
-                <div className='form__section'>
-                  <label className='form__label'>Message</label>
-                  <br />
-                  <textarea className='form__textBox' name='message' />
-                </div>
-
-                <div className='form__section'>
-                  <button className='form__btn' type='submit'>
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+        
+          
 
           {/* D E T A I L S */}
-          <div className='contact__split--details'>
-            <div className='details'>
+            <div className='details'>              
               <div className='details--detail'>
+              <a href="mailto:hello@bummings.io">
                 <img
                   className='details--detail__icon'
                   src={email}
                   alt='email'
                 />
-                <p className='details--detail__text'>hello@bummings.io</p>
+              </a>
+                <span className='details--detail__text'>hello@bummings.io</span>
               </div>
-
               
-
               <div className='details--detail'>
                 <img
                   className='details--detail__icon'
@@ -96,11 +60,9 @@ class Contact extends Component {
                 >
                   /in/eddburke/
                 </a>
-              </div>
-            </div>
+              </div>     
           </div>
         </div>
-      </div>
     );
   }
 }
